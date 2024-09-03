d51f1ec5cda5b4923e393bc665ee830f7e03b0c1
AYUSH KUMAR <85921489+Aayush27Kumar@users.noreply.github.com>
2024-09-03 16:53:49 +0530

    Update half_adderrrrr.v

--- half_adderrrrr.v
+++ half_adderrrrr.v
@@ -1,22 +1,6 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 09.08.2024 01:51:43
-// Design Name: 
-// Module Name: half_adderrrrr
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
+
 //////////////////////////////////////////////////////////////////////////////////
 
 module half_adder(input a,b,
@@ -27,4 +11,3 @@ module half_adder(input a,b,
  assign carry = a & b;
 
 endmodule
-
